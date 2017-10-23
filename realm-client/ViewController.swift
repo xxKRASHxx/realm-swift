14a7c0ab76e5eed8c646734c1f50bc7da4095301
Danil Lisovoy <danil.lisovoy@sigma.software>
2017-10-24 00:12:54 +0300

    Initial Commit

---
+++ realm-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  realm-client
+//
+//  Created by Danil Lisovoy on 10/24/17.
+//  Copyright © 2017 Danil Lisovoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
